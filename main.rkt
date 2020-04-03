7e36e60c843eeea41cbf9e0042d1df45c74eb850
0918nobita <nobita.0918@gmail.com>
2020-04-03 21:26:57 +0900

    不必要な標準出力をしないようにした

--- main.rkt
+++ main.rkt
@@ -4,24 +4,33 @@
 (require racket/gui/base)
 
 ; Make a frame by instantiating the frame% class
-(define frame (new frame% [label "Racket"] [min-width 500] [min-height 400]))
+(define frame
+  (new frame%
+       [label "Racket"]
+       [min-width 500]
+       [min-height 400]))
 
-(new canvas%
-     [parent frame]
-     [paint-callback
-      (lambda (canvas dc)
-        (send dc set-scale 3 3)
-        (send dc set-text-foreground (make-object color% 186 85 211))
-        (send dc draw-text "Hello, world!" 45 55))])
+(void
+  (new canvas%
+       [parent frame]
+       [paint-callback
+        (lambda (canvas dc)
+          (send dc set-scale 3 3)
+          (send dc set-text-foreground (make-object color% 186 85 211))
+          (send dc draw-text "Hello, world!" 45 55))]))
 
 (define msg
-  (new message% [parent frame] [label "No events so far..."]))
+  (new message%
+       [parent frame]
+       [label "No events so far..."]))
 
-(new button%
-     [parent frame]
-     [label "Click me"]
-     [callback (lambda (button event)
-                 (send msg set-label "Button click"))])
+(void
+  (new button%
+       [parent frame]
+       [label "Click me"]
+       [callback
+        (lambda (button event)
+          (send msg set-label "Button click"))]))
 
 ; Show the frame by calling its show method
 (send frame show #t)
