2a2b2f5123b03cfeb80841fcd83fdaf8514aa5c0
0918nobita <nobita.0918@gmail.com>
2020-04-04 13:25:15 +0900

    :sparkles: メニューバーにアイテムを追加した

--- main.rkt
+++ main.rkt
@@ -10,14 +10,35 @@
                   canvas%
                   color%
                   frame%
+                  menu%
+                  menu-bar%
+                  menu-item%
                   message%))
 
 ; Make a frame by instantiating the frame% class
 (define frame
   (new frame%
-       [label "Racket"]
+       [label "Game"]
        [min-width 500]
        [min-height 400]))
+
+(define menu-bar
+  (new menu-bar%
+       [parent frame]))
+
+(define menu
+  (new menu%
+       [label "File"]
+       [parent menu-bar]))
+
+(void
+  (new menu-item%
+       [label "Save"]
+       [parent menu]
+       [callback
+        (λ (menu-item control-event)
+          (printf "Event type : ~a\n" (send control-event get-event-type))
+          (send msg set-label "Saved!"))]))
 
 (void
   (new canvas%
