93953807e90bd9d301cd69dfa12f17460e4f5749
0918nobita <nobita.0918@gmail.com>
2020-07-15 09:22:13 +0900

    :building_construction: Typed Racket に関するサンプルをサブディレクトリにまとめた

--- typed_racket/typed.rkt
+++ typed_racket/typed.rkt
@@ -2,8 +2,8 @@
 
 (struct pt ([x : Real] [y : Real]))
 
-(: distance (-> pt pt Real))
-(define (distance p1 p2)
+(: distance (pt . -> .　(pt . -> . Real)))
+(define ((distance p1) p2)
   (sqrt (+ (sqr (- (pt-x p2) (pt-x p1)))
            (sqr (- (pt-y p2) (pt-y p1))))))
 
